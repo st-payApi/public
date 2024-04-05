7ee2f5673839e6990cb97e26a7edede29abe1d21
sungbly1tsstt <114800438+sungbly1tsstt@users.noreply.github.com>
2024-04-05 16:06:04 +0800

    test

--- Api/public.getLoadTime.js
+++ Api/public.getLoadTime.js
@@ -1,5 +1,7 @@
 window.onload = function () {
     var loadTime = window.performance.timing.domContentLoadedEventEnd-window.performance.timing.navigationStart; 
     console.log('Page load time is '+ loadTime);
-    return(loadTime);
+}
+function getLoadTime(){
+    return loadTime;
 }
